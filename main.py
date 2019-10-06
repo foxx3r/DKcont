541dc95c5180f301a3025aedfdfc382c16338ac8
foxx3r <coluna123@protonmail.com>
2019-10-06 01:59:11 +0000

    adicionada melhorias em relacao ao clean code e ao Zen do Python

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from art import *
+from art import tprint
 option = ficha = 0
 
 print('×'*50)
@@ -17,63 +17,34 @@ print('×'*50)
 print('▬'*15,'\033[1;34m MATEMATICA\033[m','▬'*12)
 print( '<'*8,'\033[1;32mESCOLHA UMA QUANTIDADE DE FICHAS\033[m', '>'*7)
 ficha = int(input('Digite quantas fichas voce quer: '))
-print( '<'*13,'\033[1;32mESCOLHA UMA OPÇÃO\033[m', '>'*13)
+print(f'{"<" * 13} \033[1;32mESCOLHA UMA OPÇÃO\033[m{"<" * 13}')
 
-for x in range(1,ficha +1):
-	print('''
+for x in range(1,ficha + 1):
+    print('''
 	\033[1;32m[1]MATEMATICA\033[m
 	\033[1;33m[2]FISICA\033[m
 	\033[1;36m[3]FAZER PESQUISA NO WIKIPEDIA
 	\033[1;34m[4]PARA CONTINUAR\033[m
 	\033[1;35m[5]REGRAS E INFORMACOES DO JOGO
 	\033[1;31m[6]PARA SAIR\033[m''')
-	option = int(input('\033[1;33mDigite a Opção Desejada: \033[m'))
-	if option == 1:
-		import m1
-	if option == 2:
-		import m2
-	if option == 3:
-		import m5
-	if option == 5:
-		import m4
-	if option == 6:
-		break
-		
-	
-	
-		
-		
 
+    try:
+        option = int(input('\033[1;33mDigite a Opção Desejada: \033[m'))
+    except ValueError:
+        print('Digite um inteiro apenas!')
+        exit()
+    except KeyboardInterrupt:
+        pass
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if option == 1:
+        import m1
+    if option == 2:
+        import m2
+    if option == 3:
+        import m5
+    if option == 5:
+        import m4
+    if option == 6:
+        break
+    elif option < 1 or option > 6:
+        print("Opcao invalida!")
